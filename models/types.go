4c50f4b137d5ef5bb9d425772cd080d28bf00262
Victor Santos <vsantos.py@gmail.com>
2020-09-04 00:28:08 -0300

    fix misconfigured comments

--- models/types.go
+++ models/types.go
@@ -54,26 +54,26 @@ type CreditCard struct {
 	CreatedAt  primitive.DateTime `json:"created_at,omitempty" bson:"created_at,omitempty"`
 }
 
-// Income s
+// Income defines an user outcome for a certain month
 type Income struct {
 	GrossIncome float64 `json:"gross" bson:"gross"`
 	NetIncome   float64 `json:"net" bson:"net"`
 }
 
-// Outcome d
+// Outcome defines an user outcome for a certain month
 type Outcome struct {
 	FixedOutcome   float64 `json:"fixed" bson:"fixed"`
 	DynamicOutcome float64 `json:"dynamic" bson:"dynamic"`
 }
 
-// PaymentMethod s
+// PaymentMethod defines which payment method was used for a certain spend
 type PaymentMethod struct {
 	Credit      CreditCard `json:"credit,omitempty" bson:"credit,omitempty"`
 	Debit       bool       `json:"debit,omitempty" bson:"debit,omitempty"`
 	PaymentSlip bool       `json:"payment_slip,omitempty" bson:"payment_slip,omitempty"`
 }
 
-// Spends d
+// Spends defines a user spend to be added to Balance
 type Spends struct {
 	ID            primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 	OwnerID       primitive.ObjectID `json:"owner_id,omitempty" bson:"owner_id,omitempty"`
